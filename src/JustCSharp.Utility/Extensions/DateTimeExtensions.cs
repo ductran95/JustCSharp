bc0d1a90c56a97b3fc1543ec4b019ca036fe285f
Duc Tran <tv.duc95@gmail.com>
2021-11-08 16:25:15 +0700

    Add DateTimeExtensions

---
+++ src/JustCSharp.Utility/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace JustCSharp.Utility.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static readonly TimeZoneInfo VietNameTimezone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Ho_Chi_Minh");
+        
+        public static DateTime ToVietNamTime(this DateTime utcDateTime)
+        {
+            if (utcDateTime.Kind == DateTimeKind.Local)
+            {
+                throw new ArgumentException("Need to be UTC Time");
+            }
+
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, VietNameTimezone);
+        }
+        
+        public static DateTime FromVietNamTime(this DateTime utcDateTime)
+        {
+            if (utcDateTime.Kind == DateTimeKind.Utc)
+            {
+                throw new ArgumentException("Need to be Local Time");
+            }
+
+            return TimeZoneInfo.ConvertTimeFromUtc(utcDateTime, VietNameTimezone);
+        }
+    }
+}
